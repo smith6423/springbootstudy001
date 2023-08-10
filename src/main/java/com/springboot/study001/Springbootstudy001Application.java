546e1731b87ff81c4f56a57b48da94f1145d1cfe
smith6423 <smith720@naver.com>
2023-08-10 09:31:32 +0900

    Initial commit

---
+++ src/main/java/com/springboot/study001/Springbootstudy001Application.java
@@ -0,0 +1,13 @@
+package com.springboot.study001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootstudy001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootstudy001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/study001/Springbootstudy001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.study001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootstudy001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
